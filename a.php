9018a5d4ca5e126f642e1579d08e587e8de9cead
poper-LiTengzhen <tengzhen.li@poper.co>
2026-08-05 09:05:16 +0000

    tesat

--- a.php
+++ a.php
@@ -1,3 +1,11 @@
-<?
+<?php
 
 $result = unserialize($_GET['data']);
+
+$password = unserialize($_GET['password']);
+
+insert_to_sql($password);
+
+function insert_to_sql($password){
+//todo insert sql
+};

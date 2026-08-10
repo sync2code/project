b18fdf664cb1254b0c85162d4212c1019be61697
poper-LiTengzhen <tengzhen.li@poper.co>
2026-08-10 04:23:17 +0000

    test

--- a.php
+++ a.php
@@ -2,10 +2,11 @@
 
 $result = unserialize($_GET['result'])[0]; 
 
-$password = unserialize($_GET['password']);
+$password = unserialize($_GET['token']);
 
 insert_to_sql($password);
 
 function insert_to_sql($password){
 //todo insert sql
 }
+
